d2521274b8b3bb081e75ee14953605473fe75d97
Marian Wieczorek <wieczorek@energy-robotics.com>
2021-09-15 19:46:56 +0200

    Support for preformatted markdown

--- src/components/markdown-component.tsx
+++ src/components/markdown-component.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export function parseMarkdown(text: string) {
   const gen = new IdGenerator()
-  return parseParagraphs(text, gen, (x1: string) => parseLinebreak(x1, gen, (x2: string) => parseText(x2, gen)))
+
+  const textParser = (txt: string) => parseText(txt, gen)
+  const paragraphParser = (txt: string) => parseParagraph(txt, gen, textParser)
+  return parsePreformatted(text, gen, paragraphParser)
 }
 
 type Parser = (text: string) => JSX.Element
@@ -16,18 +19,32 @@ class IdGenerator {
   }
 }
 
-function parseParagraphs(text: string, gen: IdGenerator, nextParser: Parser): JSX.Element {
-  const elements = text.split('\n\n').map((part: string) => <p key={gen.next()}>{nextParser(part)}</p>)
-  return <React.Fragment key={gen.next()}>{elements}</React.Fragment>
+function trimEmptyLines(text: string): string {
+  const lines = text.split('\n')
+  const emptyFlags = lines.map((line: string) => line.trim().length === 0)
+
+  const first = emptyFlags.indexOf(false)
+  const last = emptyFlags.lastIndexOf(false)
+
+  return lines.slice(first, last + 1).join('\n')
 }
 
-function parseLinebreak(text: string, gen: IdGenerator, nextParser: Parser): JSX.Element {
-  const elements = text.split('\n').map((part: string) => (
-    <React.Fragment key={gen.next()}>
-      {nextParser(part)}
-      <br key={gen.next()} />
-    </React.Fragment>
-  ))
+function parsePreformatted(text: string, gen: IdGenerator, nextParser: Parser) {
+  function parsePart(part: string, index: number): JSX.Element {
+    if (index % 2 === 0) {
+      return nextParser(part)
+    } else {
+      return <pre key={gen.next()}>{trimEmptyLines(part)}</pre>
+    }
+  }
+
+  const parts = text.split('```')
+  const elements = parts.map(parsePart)
+  return <>{elements}</>
+}
+
+function parseParagraph(text: string, gen: IdGenerator, nextParser: Parser): JSX.Element {
+  const elements = text.split('\n\n').map((part: string) => <p key={gen.next()}>{nextParser(part)}</p>)
   return <React.Fragment key={gen.next()}>{elements}</React.Fragment>
 }
 
